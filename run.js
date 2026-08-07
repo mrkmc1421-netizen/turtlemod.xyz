368e78ad2e37ed3c21040a6ad594be754d16fe07
mrkmc1421-netizen <mrkmc1421@gmail.com>
2026-08-07 16:44:47 -0400

    Add run button with styling in run.js

---
+++ run.js
@@ -0,0 +1,15 @@
+<button id="runButton" class="run-btn">
+  <img src="purple-flag.png" alt="Run">
+</button>
+.run-btn {
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+}
+
+.run-btn img {
+  width: 48px;
+  height: auto;
+  image-rendering: pixelated;
+}
