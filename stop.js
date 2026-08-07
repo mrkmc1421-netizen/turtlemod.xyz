a7ff5087fe1a4b298eb4eb61fad8584cb45d8c4e
mrkmc1421-netizen <mrkmc1421@gmail.com>
2026-08-07 16:45:26 -0400

    Add stop button with styling in stop.js

---
+++ stop.js
@@ -0,0 +1,15 @@
+<button id="stopButton" class="stop-btn">
+  <img src="red-prohibit.png" alt="Stop">
+</button>
+.stop-btn {
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+}
+
+.stop-btn img {
+  width: 48px;
+  height: auto;
+  image-rendering: pixelated;
+  }
